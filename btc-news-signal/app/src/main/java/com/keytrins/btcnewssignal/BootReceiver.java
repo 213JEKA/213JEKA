fd9c558e081838741717fbd19677acd0e0fb0d18
213Jeka <123963350+213JEKA@users.noreply.github.com>
2026-09-06 22:16:52 +0300

    Add BTC News Signal: app/src/main/java/com/keytrins/btcnewssignal/BootReceiver.java

---
+++ btc-news-signal/app/src/main/java/com/keytrins/btcnewssignal/BootReceiver.java
@@ -0,0 +1,11 @@
+package com.keytrins.btcnewssignal;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public final class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        AlarmScheduler.schedule(context, BlsCalendar.cached(context));
+    }
+}
